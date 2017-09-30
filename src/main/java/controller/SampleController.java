a4993b02e40fd1b9f00465f7870abb41eddf518e
daisuke-awaji <daisuke.awaji@i.softbank.jp>
2017-09-30 15:52:55 +0900

    [fix]package構成を見直す

---
+++ src/main/java/controller/SampleController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import entity.SampleEntity;
+import service.SampleService;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+	@RequestMapping("/")
+    String home(Model model) {		
+		SampleService service = new SampleService();				
+		// 1つの変数をつめる場合
+		model.addAttribute("value", "Hello World!");				
+		// List<String>だと。。。				
+		model.addAttribute("stList", service.getStringData());		
+		// List<Object>だと
+		model.addAttribute("sampleEntityList", service.getSampleEntity());		
+		
+        return "hello";
+    }
+}
--- src/main/java/entity/SampleEntity.java
+++ src/main/java/entity/SampleEntity.java
@@ -1,4 +1,4 @@
-package hello.entity;
+package entity;
 
 import lombok.Getter;
 import lombok.Setter;
